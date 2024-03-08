40c4d056d89b677e721b4a252051e962e980364b
vinimanfrin <viniciusmanfrin2019@gmail.com>
2024-03-08 20:01:30 -0300

    implementação lógica da pesquisa de imagens

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@
 
 import Template from "@/components/Template";
 import ImageCard from "@/components/ImageCard";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import Image from "@/resources/image/image.resource";
 import { useImageService } from "@/resources/image/image.service";
 
@@ -10,29 +10,32 @@ import { useImageService } from "@/resources/image/image.service";
 const Home = () => {
 
   const [images,setImages] = useState<Image[]>([])
+  const [query,setQuery] = useState<string>('')
+  const [extension,setExtension] = useState<string>('')
 
   async function getImages(){
-    const result = await useImageService().buscar();
+    const result = await useImageService().buscar(query,extension);
     setImages(result);
   }
 
   function renderImageCard(image: Image) {
-    return <ImageCard key={image.name} name={image.name} size={image.size} uploadDate={image.uploadDate} src={image.url} />
+    return <ImageCard key={image.url} name={image.name} size={image.size} uploadDate={image.uploadDate} src={image.url} />
   }
 
   function renderImages(){
     return images.map((image) => renderImageCard(image));
   }
 
-  
-
   return (
     <Template>
       <section className="flex flex-col items-center justify-center my-5">
         <div className="flex space-x-4">
-          <input type="text" className="border px-5 py-2 rounded-lg text-gray-900 shadow-lg" />
-          <select className="border px-4 py-2 rounded-lg text-gray-900 shadow-lg">
-            <option>All formats</option>
+          <input type="text" onChange={event => setQuery(event.target.value)} placeholder="Filtrar" className="border px-5 py-2 rounded-lg text-gray-900 shadow-lg" />
+          <select onChange={event => setExtension(event.target.value)} className="border px-4 py-2 rounded-lg text-gray-900 shadow-lg">
+            <option value="">Todas</option>
+            <option value="PNG">PNG</option>
+            <option value="JPEG">JPEG</option>
+            <option value="GIF">GIF</option>
           </select>
           <button className="rounded-full shadow-md px-4 py-2 bg-blue-500 text-white hover:bg-blue-600" onClick={getImages}>Pesquisar</button>
           <button className="rounded-full shadow-md px-4 py-2 bg-purple-600 text-white hover:bg-purple-700">Enviar nova Imagem</button>
