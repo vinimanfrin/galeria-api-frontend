3d77c1cb1a84824c1032e04478ffceb4d924ebbd
vinimanfrin <viniciusmanfrin2019@gmail.com>
2024-03-07 21:57:06 -0300

    criando a logica de renderização das imagens na página inicial da galeria

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,37 @@
+'use client'
+
 import Template from "@/components/Template";
 import ImageCard from "@/components/ImageCard";
+import { useEffect, useState } from "react";
+import Image from "@/resources/image/image.resource";
+import { useImageService } from "@/resources/image/image.service";
+
 
 const Home = () => {
+
+  const [images,setImages] = useState<Image[]>([])
+
+  async function getImages(){
+    const result = await useImageService().buscar();
+    setImages(result);
+  }
+
+  function renderImageCard(image: Image) {
+    return <ImageCard key={image.name} name={image.name} size={image.size} uploadDate={image.uploadDate} src={image.url} />
+  }
+
+  function renderImages(){
+    return images.map((image) => renderImageCard(image));
+  }
+
+  useEffect(() => {
+    getImages()
+  }, [])
+
   return (
     <Template>
       <section className="grid grid-cols-4 gap-8">
-        <ImageCard/>
-        <ImageCard/>
-        <ImageCard/>
+        {images.length ? renderImages() : <h1>Sem imagens disponíveis</h1>}
       </section>
     </Template>
   );
--- src/components/ImageCard.tsx
+++ src/components/ImageCard.tsx
@@ -1,5 +1,5 @@
 interface ImageCardProps {
-    title?: string;
+    name?: string;
     size?: string;
     uploadDate?: string;
     src?: string;
@@ -8,11 +8,11 @@ interface ImageCardProps {
 const ImageCard: React.FC<ImageCardProps> = (props: ImageCardProps) => {
     return (
         <div className="card relative bg-slate-200 rounded-md shadow-md transition-transform ease-in duration-300 transform hover:shadow-lg hover:-translate-y-2">
-            <img src={props.src} alt={props.title} className="h-56 w-full object-cover rounded-t-md" />
+            <img src={props.src} alt={props.name} className="h-56 w-full object-cover rounded-t-md" />
             <div className="card-body p-4">
-                <h5 className="text-xl font-semibold mb-2 text-gray-600">Nome da Imagem</h5>
-                <p className="text-gray-600">{props.size}</p>
-                <p className="text-gray-600">{props.uploadDate}</p>
+                <h5 className="text-xl font-semibold mb-2 text-gray-600">{props.name}</h5>
+                <p className="text-gray-600">Tamanho: {props.size}</p>
+                <p className="text-gray-600">Data de Upload: {props.uploadDate}</p>
             </div>
         </div>
     )
