86a188d3c37685672652272be9df006495f99b16
vinimanfrin <viniciusmanfrin2019@gmail.com>
2024-03-21 22:36:02 -0300

    implementando uma pré visualização ao selecionar uma imagem para envio

--- src/app/formulario/page.tsx
+++ src/app/formulario/page.tsx
@@ -1,6 +1,7 @@
 'use client'
 import Template from "@/components/Template";
 import { useFormik} from "formik";
+import React, { useState } from "react";
 
 interface FormProps {
     name: string;
@@ -10,12 +11,25 @@ interface FormProps {
 const formScheme: FormProps = {name: "", tags: "", file: ""}
 
 const Formulario = () => {
+
+    const [imagePreview, setImagePreview] = useState<string>("")
+
     const formik = useFormik<FormProps>({
         initialValues: formScheme,
         onSubmit: (dados: FormProps) => {
             console.log(dados)
         }
     })
+
+    const onFileUpload = (event: React.ChangeEvent<HTMLInputElement>) => {
+        if(event.target.files){
+            const file = event.target.files[0];
+            formik.setFieldValue("file", file);
+            const imageUrl = URL.createObjectURL(file);
+            setImagePreview(imageUrl)
+        }
+    }
+
     return (
         <Template>
             <div className="max-w-md mx-auto py-4">
@@ -37,10 +51,15 @@ const Formulario = () => {
                             Imagem
                         </label>
                         <div className="flex items-center border-b border-b-2 border-gray-500 py-2">
-                            <input className="appearance-none bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none" type="file" placeholder="Upload de imagem" aria-label="Image"/>
-                            <svg className="mx-auto h-12 w-12 text-gray-300" viewBox="0 0 24 24" fill="currentColor" aria-hidden="true">
-                                <path fillRule="evenodd" d="M1.5 6a2.25 2.25 0 012.25-2.25h16.5A2.25 2.25 0 0122.5 6v12a2.25 2.25 0 01-2.25 2.25H3.75A2.25 2.25 0 011.5 18V6zM3 16.06V18c0 .414.336.75.75.75h16.5A.75.75 0 0021 18v-1.94l-2.69-2.689a1.5 1.5 0 00-2.12 0l-.88.879.97.97a.75.75 0 11-1.06 1.06l-5.16-5.159a1.5 1.5 0 00-2.12 0L3 16.061zm10.125-7.81a1.125 1.125 0 112.25 0 1.125 1.125 0 01-2.25 0z" clipRule="evenodd" />
-                            </svg>
+                            <input onChange={onFileUpload} className="appearance-none bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none" type="file" placeholder="Upload de imagem" aria-label="Image"/>
+                            
+                            {imagePreview ? <img src={imagePreview} width={250} className="rounded-md"/> : 
+                                <svg className="mx-auto h-12 w-12 text-gray-300" viewBox="0 0 24 24" fill="currentColor" aria-hidden="true">
+                                    <path fillRule="evenodd" d="M1.5 6a2.25 2.25 0 012.25-2.25h16.5A2.25 2.25 0 0122.5 6v12a2.25 2.25 0 01-2.25 2.25H3.75A2.25 2.25 0 011.5 18V6zM3 16.06V18c0 .414.336.75.75.75h16.5A.75.75 0 0021 18v-1.94l-2.69-2.689a1.5 1.5 0 00-2.12 0l-.88.879.97.97a.75.75 0 11-1.06 1.06l-5.16-5.159a1.5 1.5 0 00-2.12 0L3 16.061zm10.125-7.81a1.125 1.125 0 112.25 0 1.125 1.125 0 01-2.25 0z" clipRule="evenodd" />
+                                </svg>
+                            }
+                            
+                            
                         </div>
                     </div>
                     <div className="flex items-center justify-between">
